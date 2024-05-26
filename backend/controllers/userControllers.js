2064d966d8b43c84014fb00b2de7c04e31027ef9
Zeeshan-art <zeeshanidrees899@gmail.com>
2024-05-26 11:36:47 +0500

    register api created

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { email, firstName, lastName, password } = req.body;
@@ -7,9 +8,24 @@ const register = async (req, res) => {
       email: email,
     });
     if (user) {
-      res.send("Email already exist");
+      return res.status(400).json("Email already exist");
     }
-  } catch (error) {}
+
+    user = new User(req.body);
+    await user.save();
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+    return res.status(200).send({ message: "user register successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "something went wrong" });
+  }
 };
 
 module.exports = { register };
